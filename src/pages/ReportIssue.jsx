6166985dbf8cd216f3017cc8fb876399647b3c0d
shireen <shireen.konatam9@gmail.com>
2026-03-06 18:42:01 +0530

    Firebase Connection & App Routing
    Added Firebase, AuthContext, Routing and Protected Routing

---
+++ src/pages/ReportIssue.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReportIssue = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Report Issue Page</h1>
+        </div>
+    );
+};
+
+export default ReportIssue;
